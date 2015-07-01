a8f21c8f4a72ccfa0a19e33e79f19ee20a534b90
Ruadhan O'Donoghue <ruadhan.odonoghue@gmail.com>
2015-07-01 13:41:41 +0200

    move functions into service worker

--- push/sw/sw.js
+++ push/sw/sw.js
@@ -1,1 +1,49 @@
 //sw.js
+self.addEventListener('push', function(e) {
+  console.log('push received');
+
+  //Get the notification data, then display notification
+  fetchNotDataJSONP();
+}
+
+
+function fetchNotDataJSONP() {
+  console.log('fetching JSONP...');
+  //uses jquery
+   // var url = 'http://mobiforge.com/m/subscribe.php';
+  $.getJSON("http://mobiforge.com/m/latest.php?callback=?", function(result){
+    //response data are now in the result variable
+    console.log(result);
+    showNotification(result);
+  });
+}
+
+function showNotification(notificationData) {
+  console.log('setting up notification');
+    if(Notification.permission=='granted') {
+          var notification = new Notification(notificationData.data.title, {  
+            body: notificationData.data.body,
+            icon: 'favicon.ico' 
+        }); 
+
+     notification.onclick = function() {console.log('click');
+    window.open(notificationData.data.url, '_blank');
+      //win.focus();
+        } 
+
+    }
+    else {
+      Notification.requestPermission(function(permission) {
+      if(permission=='granted') {
+        var notification = new Notification(notificationData.data.title, {  
+            body: notificationData.data.body,
+            icon: 'favicon.ico' 
+        });  
+        notification.onclick = function() {console.log('click');
+    var win = window.open(notificationData.data.url, '_blank');
+      win.focus();
+        } 
+      }          
+    });
+    }
+}
