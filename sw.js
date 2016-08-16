4bef295cf1a231491c973f75470dc81d38c56f13
Ruadhan O'Donoghue <rodonoghue@mtld.mobi>
2016-08-16 17:34:57 +0200

    Add cache policy

--- sw.js
+++ sw.js
@@ -2,6 +2,8 @@ importScripts('/lib/node_modules/sw-toolbox/sw-toolbox.js');
 
 toolbox.precache(['mf-logo.svg']);
 
+toolbox.router.default = toolbox.cacheFirst;
+
 self.addEventListener("install", function(event) {
   console.log('SW: Installing service worker');
 });
