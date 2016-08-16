1c8e8e1fae913b9ddba65a614b49b188c6524577
Ruadhan O'Donoghue <rodonoghue@mtld.mobi>
2016-08-16 17:09:43 +0200

    Precache image, add image to page

--- sw.js
+++ sw.js
@@ -1,5 +1,8 @@
 importScripts('/lib/node_modules/sw-toolbox/sw-toolbox.js');
 
+toolbox.precache(['mf-logo.svg']);
+
 self.addEventListener("install", function(event) {
   console.log('SW: Installing service worker');
 });
+
